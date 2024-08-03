9ef72bdc0f79548e3d7296f81d13f754333087a3
yneq <yneq@ms53.hinet.net>
2024-08-03 08:02:15 +0800

    Initial commit4

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from controllers import user_controller, attraction_controller, booking_controll
 app = FastAPI()
 
 # 靜態頁面
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("app1/static", StaticFiles(directory="static"), name="static")
 app.include_router(static_pages.router)
 
 # API 路由
-app.include_router(user_controller.router)
-app.include_router(attraction_controller.router)
-app.include_router(booking_controller.router)
-app.include_router(order_controller.router)
+app.include_router(static_pages.router, prefix="/app1")
+app.include_router(user_controller.router, prefix="/app1")
+app.include_router(attraction_controller.router, prefix="/app1")
+app.include_router(booking_controller.router, prefix="/app1")
+app.include_router(order_controller.router, prefix="/app1")
